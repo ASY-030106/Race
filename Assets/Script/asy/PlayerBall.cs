84e3795fff8aab4aabb24be91a2c1dd40dff88bf
asy <asy951013@naver.com>
2023-07-11 23:38:00 +0900

    0711

--- Assets/Script/asy/PlayerBall.cs
+++ Assets/Script/asy/PlayerBall.cs
@@ -7,18 +7,21 @@ public class PlayerBall : MonoBehaviour
 {
     Rigidbody rigid;
     public int cnt;
+    bool isJump;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody>();
         cnt = 0;
+        isJump = false;
     }
 
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && !isJump) //isJump가 true면
         {
-            rigid.AddForce(new Vector3(0,40,0),ForceMode.Impulse);
+            isJump = true;
+            rigid.AddForce(new Vector3(0,50,0),ForceMode.Impulse);
         }
     }
 
@@ -26,16 +29,15 @@ public class PlayerBall : MonoBehaviour
     {
         float ho = Input.GetAxisRaw("Horizontal"); //오른쪽 왼쪽
         float ve = Input.GetAxisRaw("Vertical"); // 위쪽 아래쪽
-        rigid.AddForce(new Vector3(ho, 0,ve), ForceMode.Impulse);
+        rigid.AddForce(new Vector3(ho, 0,ve), ForceMode.Impulse); //순간적인 힘으로 무게를 적용할 때
     }
 
-    void OnTriggerEnter(Collider other) //다른 객체와 충돌했을 때 , ohter이 다른 객체를 의미?
+    void OnCollisionEnter(Collision collision)
     {
-        if (other.tag == "Item")
+        if(collision.gameObject.name == "Floor")
         {
-            cnt++;
-            Debug.Log("점수 : " + cnt);
-            other.gameObject.SetActive(false);
+            Debug.Log("Floor에 닿음");
+            isJump = false;
         }
     }
 }
